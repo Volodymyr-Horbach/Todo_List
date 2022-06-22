6548957ca22546fe712f50a002f6cf65e0e7c9f5
myusername <103590333+Volodymyr-Horbach@users.noreply.github.com>
2022-06-22 21:22:14 +0300

    first commit

---
+++ src/Component/ToDoForm.js
@@ -0,0 +1,30 @@
+import {useState} from 'react'
+
+
+function ToDoForm({addTask}){
+    const [userInput, setUserInput] = useState()
+
+    const handleChange = (e) => {
+        setUserInput(e.currentTarget.value)
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        addTask(userInput)
+        setUserInput('')
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input 
+            value={userInput}
+            type="text"
+            onChange={handleChange}
+            placeholder="Enter task..."
+            />
+            <button>Add</button>
+        </form>
+    ) 
+}
+
+export default ToDoForm;
